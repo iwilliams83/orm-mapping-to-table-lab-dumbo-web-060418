7b3f3f28e1f1ca69757ad9df49994bcd4c5b0fe6
iwilliams83 <indirajamurath@gmail.com>
2018-06-12 21:43:32 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -34,14 +34,3 @@ class Student
   end
 
 end
-
-# def self.create_table
-#    sql =  <<-SQL
-#      CREATE TABLE IF NOT EXISTS songs (
-#        id INTEGER PRIMARY KEY,
-#        name TEXT,
-#        album TEXT
-#        )
-#        SQL
-#    DB[:conn].execute(sql)
-#  end
